01856133ac2eb59de6b2ab64a752440aee3f7a70
lihangyu <lihangyu@youzan.com>
2026-08-14 11:43:47 +0800

    Initial Commit

---
+++ easy-webui/easy_webuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  easy_webuiApp.swift
+//  easy-webui
+//
+//  Created by lihangyu on 2026/8/14.
+//
+
+import SwiftUI
+
+@main
+struct easy_webuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
